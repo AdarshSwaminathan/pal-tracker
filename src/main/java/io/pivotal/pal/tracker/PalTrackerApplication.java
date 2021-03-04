b8805e5cc74d5469305e405af6d055dc9f0e835c
AdarshSwaminathan <adarshswaminathan@gmail.com>
2021-03-04 08:51:58 +0000

    CF - Spring JDBC Template - DAy3

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
@@ -14,5 +18,9 @@ public class PalTrackerApplication {
     @Bean
     public TimeEntryRepository getTimeEntryRepository() {
         return new InMemoryTimeEntryRepository();
+    }
+
+    public PalTrackerApplication(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 }
